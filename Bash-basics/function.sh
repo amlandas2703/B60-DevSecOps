41768df8b2b9615e6918a97ff2ec5bd2b6390fd2
amlandas2703 <amlandas2703@gmail.com>
2026-05-06 01:51:25 +0530

    added

--- Bash-basics/function.sh
+++ Bash-basics/function.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 var1=$1
 var2=$2
+echo "The number of arguements used is $@"
 step ()
 {
     echo "Hello everyone"
     echo "The script name is $0"
-    echo "The number of arguements used is $@"
+    
 }
 
 step
