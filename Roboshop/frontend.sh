b8e5c38a46f7664db6ed9ae16aedd6a2bde08c4f
amlandas2703 <amlandas2703@gmail.com>
2026-05-08 08:51:49 +0530

    added com

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -7,3 +7,23 @@ if [ $USER -ne 0 ];then
 else
     echo -e "\e[33m Welcome to the script \e[0m"
 fi
+
+stat(){
+    if [ $1 -eq 0 ];then
+        echo -e "\e[32m Success \e[0m"
+    else    
+        echo -e "\e[31m Failure \e[0m"
+}
+
+
+echo -n "Disabling the nginx repo"
+dnf module disable nginx -y
+stat $?
+
+echo -n "Enabling nginx:1.24 repo"
+dnf module enable nginx:1.24 -y
+stat $?
+
+echo -n "installing nginx"
+dnf install nginx -y
+stat $?
