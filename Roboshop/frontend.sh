615a17f25cb0b1340b3fe308a5800bbf4de8132c
amlandas2703 <amlandas2703@gmail.com>
2026-05-10 12:59:43 +0530

    added

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-USER=$(id -u)
+ID=$(id -u)
+COMPONENT= "frontend"
+LOG="/tmp/${COMPONENT}.log"
 
-if [ $USER -ne 0 ];then
-    echo -e "\e[32m You should run the script as a root \e[0m"
-else
-    echo -e "\e[33m Welcome to the script \e[0m"
+if [ $ID -ne 0 ];then
+    echo -e "\e[33m Login as Root user or type sudo command \e[0m"
+    exit 1
 fi
 
 stat()
 {
     if [ $1 -eq 0 ];then
         echo -e "\e[32m Success \e[0m"
-    else    
+    else
         echo -e "\e[31m Failure \e[0m"
-    fi    
-}
+        exit 2
+    fi
+} 
 
-
-echo -n "Disabling the nginx repo"
-dnf module disable nginx -y
+echo -e "\e[32m Disabling nginx default package version \e[0m"
+dnf module disable nginx -y > $LOG
 stat $?
 
-echo -n "Enabling nginx:1.24 repo"
-dnf module enable nginx:1.24 -y
+echo -e "\e[32m Enabling nginx package version \e[0m"
+dnf module enable nginx:1.24 -y >> $LOG
 stat $?
 
-echo -n "installing nginx"
-dnf install nginx -y
+echo -e "\e[32m installing nginx package \e[0m"
+dnf install nginx -y >> $LOG
 stat $?
