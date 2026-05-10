0e1bf883855a61dc6e15bfcc6fba9db7477e0fb5
amlandas2703 <amlandas2703@gmail.com>
2026-05-10 14:00:12 +0530

    added

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -46,7 +46,6 @@ unzip -o /tmp/${COMPONENT}.zip -d /usr/share/nginx/html/ &>> $LOG
 stat $?
 
 echo -n "Copying the configuration file: "
-rm -rf /etc/nginx/nginx.conf
 cp nginx.conf /etc/nginx/nginx.conf
 stat $?
 
