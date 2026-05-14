ba8ad34b13cefe92b0e39f3774672934773396ea
amlandas2703 <amlandas2703@gmail.com>
2026-05-15 02:30:30 +0530

    added

--- Roboshop/09_rabbitmq.sh
+++ Roboshop/09_rabbitmq.sh
@@ -2,17 +2,17 @@
 COMPONENT="rabbitmq"
 source ./common.sh
 
-echo -n "Configuring the service: "
+echo -n "Configuring the repo: "
 cp /home/ec2-user/B60-DevSecOps/Roboshop/${COMPONENT}.repo /etc/yum.repos.d/${COMPONENT}.repo &>> $LOG
 stat $?
 
 echo -n "Installing $COMPONENT: "
-dnf install rabbitmq-server -y
+dnf install ${COMPONENT}-server -y
 stat $?
 
 echo -n "Enabling and starting the service: "
-systemctl start $COMPONENT &>> $LOG
-systemctl enable $COMPONENT &>> $LOG
+systemctl enable ${COMPONENT}-server 
+systemctl start ${COMPONENT}-server
 stat $?
 
 echo -n "Adding $APPUSER: "
