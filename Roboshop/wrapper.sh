b40d5549f9a6a430ac8f667c67a831c357b17299
amlandas2703 <amlandas2703@gmail.com>
2026-05-17 21:50:48 +0530

    added

--- Roboshop/wrapper.sh
+++ Roboshop/wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-COMPONENT=$1
-ENVIRONMEMT=$2
+component=$1
+environment=$2
 
 if [ -z "$1" ] && [ -z "$2" ];then
     echo -e "\e[33m Expected Usage \n \t : sudo bash $0 ComponentName EnvironmentName \e[0m"
