74a2741110e72d274b65f108db17a60316ea93e8
amlandas2703 <amlandas2703@gmail.com>
2026-06-02 06:34:18 +0530

    added

---
+++ roboshop-terraform/ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "dasa_test" {
+    ami = "ami-0fcc78c828f981df2"
+    instance_type = "t3.micro"
+
+    tags = {
+      name = "demo_machine123"
+    }
+}
+
+output "private_ip_address" {
+    value = aws_instance.dasa_test.private_ip 
+}
