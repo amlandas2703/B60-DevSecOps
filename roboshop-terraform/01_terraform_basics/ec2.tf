b0034223ac6ad5a5610c55fb685fc6837c4066fa
amlandas2703 <amlandas2703@gmail.com>
2026-06-05 17:11:01 +0530

    added

--- roboshop-terraform/01_terraform_basics/ec2.tf
+++ roboshop-terraform/01_terraform_basics/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "dasa_test" {
-    ami = "ami-0fcc78c828f981df2"
+    ami           = "ami-0fcc78c828f981df2"
     instance_type = "t3.micro"
 
     tags = {
-      name = "demo_machine123"
+      Name = "demo_machine123"
     }
 }
 
