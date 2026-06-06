e874ddb11cf82a28bc432fa7bc1629601617346e
amlandas2703 <amlandas2703@gmail.com>
2026-06-06 17:32:27 +0530

    added

--- roboshop-terraform/04_functions/vars.tf
+++ roboshop-terraform/04_functions/vars.tf
@@ -1,22 +1,22 @@
 fruits = {
-    apple = {
-        color = "red"
-        taste = "sweet"
-        price = 1.5
-        quantity = 10
-    }
-    banana = {
-        color = "yellow"
-        taste = "sweet"
-        price = 0.5
-        quantity = 20
-    }
-    orange = {
-        color = "orange"
-        taste = "citrus"
-        price = 1.0
-        quantity = 15
-    }
+  apple = {
+    color    = "red"
+    taste    = "sweet"
+    price    = 1.5
+    quantity = 10
+  }
+  banana = {
+    color    = "yellow"
+    taste    = "sweet"
+    price    = 0.5
+    quantity = 20
+  }
+  orange = {
+    color    = "orange"
+    taste    = "citrus"
+    price    = 1.0
+    quantity = 15
+  }
 }
 
 
