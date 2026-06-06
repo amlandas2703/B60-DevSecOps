7fa9a3a3e1a3eea3af4d95edee022cfbc7aa8e69
amlandas2703 <amlandas2703@gmail.com>
2026-06-06 18:59:08 +0530

    added

--- roboshop-terraform/04_functions/sample.tf
+++ roboshop-terraform/04_functions/sample.tf
@@ -2,5 +2,19 @@ variable "fruits" {}
 
 output "fruits" {
   value = var.fruits
+}
+
+
+
+output "appleproperties" {
+  value = "The color of apple is ${var.fruits["apple"].color}, its taste is ${var.fruits["apple"].taste}, its price is ${var.fruits["apple"].price} dollars and its quantity is ${var.fruits["apple"].quantity} and it is from ${var.fruits["apple"].state}"
+
+}
+
+output "bananaproperties" {
+  value = "The color of banana is ${var.fruits["banana"].color}, its taste is ${var.fruits["banana"].taste}, its price is ${var.fruits["banana"].price} dollars and its quantity is ${var.fruits["banana"].quantity} and it is from ${var.fruits["banana"].state}"
+  
+}
+
   
 }
--- roboshop-terraform/04_functions/vars.tf
+++ roboshop-terraform/04_functions/vars.tf
@@ -1,33 +1,23 @@
 fruits = {
-  apple = {
-    color    = "red"
-    taste    = "sweet"
-    price    = 1.5
-    quantity = 10
-  }
-  banana = {
-    color    = "yellow"
-    taste    = "sweet"
-    price    = 0.5
-    quantity = 20
-  }
-  orange = {
-    color    = "orange"
-    taste    = "citrus"
-    price    = 1.0
-    quantity = 15
-  }
+    
+    apple = {
+        color = "red"
+        taste = "sweet"
+        price = 1.5
+        quantity = 10
+        state = "Kashmir"
+    }
+    mango = {
+        color = "yellow"
+        taste = "sweet"
+        price = 2.0
+        quantity = 5
+    }
+    banana = {
+        color = "yellow"
+        taste = "sweet"
+        price = 0.5
+        quantity = 20
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
