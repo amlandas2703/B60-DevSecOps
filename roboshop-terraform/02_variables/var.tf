af1343b8b12ca7b98e9e79fc66d79e2b881e0756
amlandas2703 <amlandas2703@gmail.com>
2026-06-06 13:25:14 +0530

    added

--- roboshop-terraform/02_variables/var.tf
+++ roboshop-terraform/02_variables/var.tf
@@ -38,3 +38,45 @@ output "my_score" {
   value = "My score in hindi is ${var.score["hindi"]}"
   
 }
+
+
+variable "fruits_details" {
+  description = "A map of fruits and their details"
+  type        = map(object({
+    color  = string
+    taste  = string
+    price  = number
+    quantity   = number 
+  }))
+  default     = {
+    apple = {
+      color = "red"
+      taste = "sweet"
+      price = 1.5
+      quantity = 10
+    }
+    banana = {
+      color = "yellow"
+      taste = "sweet"
+      price = 0.5
+      quantity = 20
+    }
+    orange = {
+      color = "orange"
+      taste = "citrus"
+      price = 1.0
+      quantity = 15
+    }
+  }
+  
+}
+
+output "name_and_price" {
+  value = "The price of ${var.fruits_details["apple"].color} apple is ${var.fruits_details["apple"].price} dollars"
+  
+}
+
+output "name_and_quantity" {
+  value = "The quantity of ${var.fruits_details["banana"].color} banana is ${var.fruits_details["banana"].quantity}"
+  
+}
