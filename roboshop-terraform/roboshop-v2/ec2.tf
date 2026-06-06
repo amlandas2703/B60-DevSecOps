b6440372aab459a594017c3bdb629bf3f0362082
amlandas2703 <amlandas2703@gmail.com>
2026-06-06 20:57:05 +0530

    added

--- roboshop-terraform/roboshop-v2/ec2.tf
+++ roboshop-terraform/roboshop-v2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
 
     for_each      = var.components
     ami           = each.value["ami_id"]
-    instance_type = each.value["instance_type"]
+    instance_type = lookup(each.value, "instance_type", "t3.medium") 
 
     tags = {
       Name = each.key
