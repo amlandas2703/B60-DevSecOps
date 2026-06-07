6ae142546e7483e04e657c5e7a06187474d1a60e
amlandas2703 <amlandas2703@gmail.com>
2026-06-08 01:23:10 +0530

    added

--- roboshop-terraform/roboshop-v5_with_multi_env/main.tf
+++ roboshop-terraform/roboshop-v5_with_multi_env/main.tf
@@ -5,6 +5,7 @@ module "ec2" {
   env_name = var.env_name
   ami_name = var.ami_name
   sg_name = var.sg_name
+  domain_name = var.domain_name
   name = each.key
   instance_type = each.value["instance_type"]
   
