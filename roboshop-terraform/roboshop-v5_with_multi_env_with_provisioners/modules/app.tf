8296f724c9b48372c28e5409e5990444600a859c
amlandas2703 <amlandas2703@gmail.com>
2026-06-12 05:52:52 +0530

    added

--- roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/modules/app.tf
+++ roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/modules/app.tf
@@ -11,9 +11,12 @@ resource "null_resource" "app" {
             host= aws_instance.main.private_ip
         }
         inline = [
-            "pip3.11 install ansible",
-            "type ansible",
-            "ansible-pull -U https://github.com/amlandas2703/B60-DevSecOps.git roboshop-ansible/roboshop-pull.yml -e env=${var.env_name} -e component=${var.name}"
+               "which python3.11 || sudo yum install -y python3.11",
+               "which pip3.11 || sudo yum install -y python3.11-pip",
+               "pip3.11 install --user ansible",
+               "export PATH=$PATH:~/.local/bin",
+               "ansible --version",
+               "ansible-pull -U https://github.com/amlandas2703/B60-DevSecOps.git roboshop-ansible/roboshop-pull.yml -e env=${var.env_name} -e component=${var.name}"
         ]
     }
   
