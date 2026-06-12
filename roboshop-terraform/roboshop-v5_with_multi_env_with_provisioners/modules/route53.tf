958d8436616081c594c9565fd2772c94772343e7
amlandas2703 <amlandas2703@gmail.com>
2026-06-12 07:56:13 +0530

    added

--- roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/modules/route53.tf
+++ roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/modules/route53.tf
@@ -1,4 +1,5 @@
 resource "aws_route53_record" "public" {
+  for_each = aws_instance.main
   zone_id = data.aws_route53_zone.domain.zone_id
   name    = "${var.name}-${var.env_name}"
   type    = "A"
