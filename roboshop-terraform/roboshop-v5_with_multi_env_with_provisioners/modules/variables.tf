f973aec1dc39b4ff1e8c57edb8ca5f699398444d
amlandas2703 <amlandas2703@gmail.com>
2026-06-13 21:11:07 +0530

    added

--- roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/modules/variables.tf
+++ roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/modules/variables.tf
@@ -5,5 +5,16 @@ variable "instance_type" {}
 variable "name" {}
 variable "domain_name" {}
 variable "internal" {}
+variable "components" {
+  type = map(any)
+}
+
+variable "env_name" {
+  type = string
+}
+
+variable "ami_name" {
+  type = string
+}
 
 
--- roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/vars.tf
+++ roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/vars.tf
@@ -3,3 +3,14 @@ variable "ami_name" {}
 variable "sg_name" {}
 variable "components" {}
 variable "domain_name" {}
+variable "components" {
+  type = map(any)
+}
+
+variable "env_name" {
+  type = string
+}
+
+variable "ami_name" {
+  type = string
+}
