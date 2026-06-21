5c459e0d0a9ddbc525fe13b60e4f457496e84e05
amlandas2703 <amlandas2703@gmail.com>
2026-06-21 22:03:49 +0530

    added

--- roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/init.sh
+++ roboshop-terraform/roboshop-v5_with_multi_env_with_provisioners/init.sh
@@ -2,5 +2,7 @@
 action=$1
 env=$2
 token=$3
+rm -rf .terraform;
+git pull;
 terraform init --backend-config=env/"${env}"/state.tfvars; 
-terraform "${action}" --var-file=env/"${env}"/"${env}".tfvars -var vault_token=$token
+terraform "${action}" --var-file=env/"${env}"/"${env}".tfvars -var vault_token=$token -auto-approve
