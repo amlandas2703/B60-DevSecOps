7c655104db4e240b3d937a68f38df0a8bd0a4e26
amlandas2703 <amlandas2703@gmail.com>
2026-06-21 07:38:20 +0530

    added

--- roboshop-terraform/roboshop-v6_with_EKS/init.sh
+++ roboshop-terraform/roboshop-v6_with_EKS/init.sh
@@ -2,4 +2,4 @@
 action=$1
 env=$2
 terraform init --backend-config=env/"${env}"/state.tfvars; 
-terraform "${action}" --var-file=env/"${env}"/"${env}".tfvars --auto-approve;
+terraform "${action}" --var-file=env/"${env}"/"${env}".tfvars --auto-approve
