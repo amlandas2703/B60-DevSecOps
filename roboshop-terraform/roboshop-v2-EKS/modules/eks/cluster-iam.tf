5c3e34fef2daae22a64f6754c6832225d5d2b27e
amlandas2703 <amlandas2703@gmail.com>
2026-06-24 16:43:07 +0530

    added

--- roboshop-terraform/roboshop-v2-EKS/modules/eks/cluster-iam.tf
+++ roboshop-terraform/roboshop-v2-EKS/modules/eks/cluster-iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "cluster" {
-  name = "eks-cluster-example"
+  name = "${var.cluster_name}-${var.env}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
--- roboshop-terraform/roboshop-v2-EKS/modules/eks/nodegroup.tf
+++ roboshop-terraform/roboshop-v2-EKS/modules/eks/nodegroup.tf
@@ -1,7 +1,7 @@
-resource "aws_eks_node_group" "example" {
+resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.cluster_name}-${var.env}-node-group"
-  node_role_arn   = aws_iam_role.example.arn
+  node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = var.subnet_ids
   instance_types = var.instance_types
 
