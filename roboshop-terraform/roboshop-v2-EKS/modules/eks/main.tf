2ef14ee7253afe57d4a0af18542c441ead0d9a7e
amlandas2703 <amlandas2703@gmail.com>
2026-07-05 21:13:22 +0530

    added

--- roboshop-terraform/roboshop-v2-EKS/modules/eks/main.tf
+++ roboshop-terraform/roboshop-v2-EKS/modules/eks/main.tf
@@ -22,3 +22,13 @@ resource "aws_eks_cluster" "main" {
   ]
 }
 
+resource "aws_eks_addon" "cni" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "vpc-cni"
+  configuration_values = jsonencode({
+     "enableNetworkPolicy" = "true"
+     "nodeAgent": {
+        "enablePolicyEventLogs" = "true"
+     }
+    })
+}
